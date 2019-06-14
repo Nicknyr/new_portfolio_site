22da160c6fc7eb19763052795778621abd77309c
Nick <nick_kinlen@hotmail.com>
2019-06-14 15:57:57 -0400

    Added project links to projects, added Form Carry form submission for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {
@@ -12,46 +12,74 @@ import {
 import { Container, Row, Col } from 'reactstrap';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
+import axios from 'axios';
 
-const Contact = (props) => {
-  return (
-    <Fade bottom>
-    <Container>
-      <Row className="contact" id="contact">
-        <Col xs={{size: 10, offset: 1}} md={{size: 6}}>
-          <Fade left>
-          <h3 className="h3">Contact Me</h3>
-            <FontAwesomeIcon
-                  icon={faEnvelope}
-                  size="2x"
-                  color="snow"
-                />
-          <Form>
-          <FormGroup>
-            <Label for="name-label">Name</Label>
-            <Input type="text" name="name" id="name" placeholder="Enter your name" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="email-label">Email</Label>
-            <Input type="email" name="email" id="email" placeholder="Enter your email address" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="text-area-label">Your Message</Label>
-            <Input type="textarea" name="text" id="message" rows="5" />
-          </FormGroup>
-          <Button id="contact-button">Submit</Button>
-        </Form>
-        </Fade>
-      </Col>
-      <Col xs={{size: 10, offset: 1}} md={{size: 4}}>
-        <Fade right>
-        <h2 className="linked-in">Or get in touched on <a href="/">LinkedIn</a></h2>
-        </Fade>
-      </Col>
-      </Row>
-    </Container>
-    </Fade>
-  );
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
+    };
+  }
+
+  handleForm = e => {
+   axios.post(
+     "https://formcarry.com/s/VphZwAcq6nJ",
+     this.state,
+     {headers: {"Accept": "application/json"}}
+     )
+     .then(function (response) {
+       console.log(response);
+     })
+     .catch(function (error) {
+       console.log(error);
+     });
+
+   e.preventDefault();
+ }
+
+ handleFields = e => this.setState({ [e.target.name]: e.target.value });
+
+
+  render() {
+    return (
+      <Fade bottom>
+      <Container>
+        <Row className="contact" id="contact">
+          <Col xs={{size: 10, offset: 1}} md={{size: 8, offset: 2}} lg={{size: 6, offset: 3}}>
+            <Fade left>
+            <h3 className="h3">Contact Me</h3>
+              <FontAwesomeIcon
+                    icon={faEnvelope}
+                    size="2x"
+                    color="snow"
+                  />
+            <Form action="https://formcarry.com/s/VphZwAcq6nJ" method="POST" accept-charset="UTF-8">
+            <FormGroup>
+              <Label for="name-label">Name</Label>
+              <Input type="text" name="name" id="name" placeholder="Enter your name" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="email-label">Email</Label>
+              <Input type="email" name="email" id="email" placeholder="Enter your email address" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="text-area-label">Your Message</Label>
+              <Input value={this.state.value} onChange={this.handleChange} type="textarea" name="text" id="message" rows="5" />
+            </FormGroup>
+            <Button id="contact-button">Submit</Button>
+          </Form>
+          </Fade>
+        </Col>
+        </Row>
+      </Container>
+      </Fade>
+    );
+  }
+
 }
 
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -18,7 +18,11 @@ import Fade from 'react-reveal/Fade';
 const Portfolio = (props) => {
   return (
       <Container>
-      <h3 className="h3 project-h3">Projects</h3>
+      <Row>
+        <Fade bottom>
+          <h3 className="h3 portfolio-h3">Projects</h3>
+        </Fade>
+      </Row>
       <Row className="portfolio" id="portfolio">
         <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg={{size: 4, offset: 0}}>
           <Fade bottom>
@@ -26,7 +30,9 @@ const Portfolio = (props) => {
               <CardHeader className="project-header">Steemit.com Top Posts</CardHeader>
               <CardBody>
                 <CardText className="project-text">A React and Redux web app that uses the dSteem API to pull social media posts from the Steem blockchain</CardText>
-                <Button className="project-button">View Online</Button>
+                <a href="https://steem-project.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                  <Button className="project-button">View Online</Button>
+                </a>
                 <CardImg bottom src={Steem} height={200} width={200} alt="Card image cap" />
               </CardBody>
             </Card>
@@ -35,10 +41,12 @@ const Portfolio = (props) => {
         <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg={{size: 4, offset: 0}}>
           <Fade bottom>
             <Card className="portfolio-project">
-              <CardHeader className="project-header">Bitcoin Price Line Graph</CardHeader>
+              <CardHeader className="project-header">Bitcoin Price Graph</CardHeader>
               <CardBody>
                 <CardText className="project-text">A D3.js line graph that makes a call to the CryptoCompare API and plots Bitcoin prices</CardText>
-                <Button className="project-button">View Online</Button>
+                <a href="https://codepen.io/nick_kinlen/full/yEYmxy/" target="_blank" rel="noopener noreferrer">
+                  <Button className="project-button">View Online</Button>
+                </a>
                 <CardImg bottom src={Bitcoin} height={200} width={200} alt="Card image cap" />
               </CardBody>
             </Card>
@@ -50,7 +58,9 @@ const Portfolio = (props) => {
               <CardHeader className="project-header">European GDP Map</CardHeader>
               <CardBody>
                 <CardText className="project-text">Mapbox choropleth map that uses Leaflet.js and GeoJSON data to plot GDP by nation</CardText>
-                <Button className="project-button">View Online</Button>
+                <a href="https://eurogdp.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                  <Button className="project-button">View Online</Button>
+                </a>
                 <CardImg bottom src={EuroMap} height={200} width={200} alt="Card image cap" />
               </CardBody>
             </Card>
@@ -62,7 +72,9 @@ const Portfolio = (props) => {
             <CardHeader className="project-header">React Recipe App</CardHeader>
             <CardBody>
               <CardText className="project-text">React.js app that allows the user to view, add, edit, and delete recipes in a To-Do-List style App</CardText>
-              <Button className="project-button">View Online</Button>
+              <a href="https://fcc-recipebox.surge.sh/" target="_blank" rel="noopener noreferrer">
+                <Button className="project-button">View Online</Button>
+              </a>
               <CardImg bottom src={RecipeBox} height={200} width={200} alt="Card image cap" />
             </CardBody>
             </Card>
@@ -74,7 +86,9 @@ const Portfolio = (props) => {
               <CardHeader className="project-header">JQuery Pomodoro Clock</CardHeader>
               <CardBody>
                 <CardText className="project-text">A classic Pomodoro clock built with Jquery</CardText>
-                <Button className="project-button">View Online</Button>
+                <a href="https://pomodoro-clock-fcc.surge.sh/" target="_blank" rel="noopener noreferrer">
+                  <Button className="project-button">View Online</Button>
+                </a>
                 <CardImg bottom src={Pomodoro} height={200} width={200} alt="Card image cap" />
               </CardBody>
             </Card>
@@ -83,10 +97,12 @@ const Portfolio = (props) => {
         <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg={{size: 4, offset: 0}}>
           <Fade bottom>
             <Card className="portfolio-project">
-              <CardHeader className="project-header">NASA & SpaceX Launch Map</CardHeader>
+              <CardHeader className="project-header">NASA & SpaceX Map</CardHeader>
               <CardBody>
                 <CardText className="project-text">A Mapbox world map that plots NASA and SpaceX facilities using React-Leaflet, an official NASA API, and a SpaceX API</CardText>
-                <Button className="project-button">View Online</Button>
+                <a href="https://launchfacilities.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                  <Button className="project-button">View Online</Button>
+                </a>
                 <CardImg bottom src={SpaceX} height={200} width={200} alt="Card image cap" />
               </CardBody>
             </Card>
